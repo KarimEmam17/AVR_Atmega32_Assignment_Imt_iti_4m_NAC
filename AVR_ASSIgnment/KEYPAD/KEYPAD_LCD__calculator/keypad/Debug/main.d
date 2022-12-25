d9bb755a8fc35b99bc746926ba5ade5f666ec5fa
Karim  Mohamed Emam <104641970+KarimEmam17@users.noreply.github.com>
2022-12-25 05:04:30 +0200

    Add files via upload
    calculator bt 4*4 keypad  and LCD showing  floating numbers

---
+++ AVR_ASSIgnment/KEYPAD/KEYPAD_LCD__calculator/keypad/Debug/main.d
@@ -0,0 +1,15 @@
+main.o main.o: ../main.c ../../STD_LIB/STD_Types.h ../../STD_LIB/util.h \
+  ../keypad_interface.h ../../MCAL/DIO/DIO_interface.h ../LCD_INTERFACE.h \
+  ../LCD_CONFIG.h
+
+../../STD_LIB/STD_Types.h:
+
+../../STD_LIB/util.h:
+
+../keypad_interface.h:
+
+../../MCAL/DIO/DIO_interface.h:
+
+../LCD_INTERFACE.h:
+
+../LCD_CONFIG.h:
